c8cb165e483d793ea8bfb326686ce8cebc4b6904
prisme <jeremy.saintprix@gmail.com>
2017-03-17 01:37:29 +0100

    npm i soundcloud-audio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('hello voyeur')
+var SoundCloudAudio = require('soundcloud-audio');
+var scPlayer = new SoundCloudAudio('a513ea843cd798d72b39eeb91cad08a1');
+
+scPlayer.resolve('http://soundcloud.com/voyeurmusic/sets/vm017', function (playlist) {
+    // do smth with array of `playlist.tracks` or playlist's metadata
+    // e.g. display playlist info in a view etc.
+    console.log(playlist);
+
+    // once playlist is loaded it can be played
+    scPlayer.play();
+
+    // for playlists it's possible to switch to another track in queue
+    // e.g. we do it here when playing track is finished
+    scPlayer.on('ended', function () {
+        scPlayer.next();
+    });
+
+    setTimeout(function(){scPlayer.pause()}, 7777)
+
+    // play specific track from playlist by it's index
+    // scPlayer.play({playlistIndex: 2});
+});
