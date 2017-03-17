96db6b9711778a12a58a0505d0b0eecff598081a
prisme <jeremy.saintprix@gmail.com>
2017-03-17 13:15:54 +0100

    btn-playpause : basic style & functionnality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 var SoundCloudAudio = require('soundcloud-audio');
 var scPlayer = new SoundCloudAudio('a513ea843cd798d72b39eeb91cad08a1');
+var Hammer = require('hammerjs')
+
+
 
 if (document.querySelector('.player') != null) {
     var ep_url = document.querySelector('.btn-primary.soundcloud').href
@@ -8,7 +11,7 @@ if (document.querySelector('.player') != null) {
         console.log(playlist);
 
         // once playlist is loaded it can be played
-        scPlayer.play();
+        // scPlayer.play();
 
         // for playlists it's possible to switch to another track in queue
         // e.g. we do it here when playing track is finished
@@ -21,4 +24,25 @@ if (document.querySelector('.player') != null) {
         // play specific track from playlist by it's index
         // scPlayer.play({playlistIndex: 2});
     });
+
+
+
+    var btnPlayPause = document.querySelector('.btn-playpause')
+    var mc = new Hammer(btnPlayPause)
+    mc.on("tap press", function(ev) {
+        var element = ev.target.parentElement
+        var state = ev.target.parentElement.classList.contains('playing') ? 'playing' : 'paused'
+
+        if( state == 'playing' )
+            scPlayer.pause()
+        else
+            scPlayer.play()
+
+        element.classList.toggle('paused')
+        element.classList.toggle('playing')
+    })
+
+    // btnPlayPause.addEventListener('click', function(){
+    //     console.log('btnPlayPause')
+    // })
 }
