482f6524e832e50acb003801c9dbb0c253a49816
prisme <jeremy.saintprix@gmail.com>
2017-03-17 21:06:02 +0100

    √

--- src/bundle.js
+++ src/bundle.js
@@ -2867,49 +2867,63 @@ var SoundCloudAudio = require('soundcloud-audio');
 var scPlayer = new SoundCloudAudio('a513ea843cd798d72b39eeb91cad08a1');
 var Hammer = require('hammerjs')
 
-
-
 if (document.querySelector('.player') != null) {
-    var ep_url = document.querySelector('.btn-primary.soundcloud').href
+    var url = document.querySelector('.btn-primary.soundcloud').href
 
-    scPlayer.resolve(ep_url, function (playlist) {
-        console.log(playlist);
-
-        // once playlist is loaded it can be played
-        // scPlayer.play();
-
-        // for playlists it's possible to switch to another track in queue
-        // e.g. we do it here when playing track is finished
-        scPlayer.on('ended', function () {
-            scPlayer.next();
-        });
-
-        // setTimeout(function(){scPlayer.pause()}, 7777)
-
-        // play specific track from playlist by it's index
-        // scPlayer.play({playlistIndex: 2});
-    });
-
-
-
+    // elts
     var btnPlayPause = document.querySelector('.btn-playpause')
-    var mc = new Hammer(btnPlayPause)
-    mc.on("tap press", function(ev) {
-        var element = ev.target.parentElement
-        var state = ev.target.parentElement.classList.contains('playing') ? 'playing' : 'paused'
+    var btnTrackCollection = document.querySelectorAll('.ep-track')
+    var btnTrackArr = []; for(var i = btnTrackCollection.length; i--; btnTrackArr.unshift(btnTrackCollection[i]));
 
-        if( state == 'playing' )
+    // state
+    var activeTrack = 0
+    var isPlaying = 0
+
+    var playPause = function(ev) {
+        if( isPlaying ){
             scPlayer.pause()
-        else
-            scPlayer.play()
+        }
+        else {
+            scPlayer.play({playlistIndex: activeTrack})
+            btnTrackArr[activeTrack].classList.add('active')
+        }
 
-        element.classList.toggle('paused')
-        element.classList.toggle('playing')
-    })
+        isPlaying = !isPlaying
 
-    // btnPlayPause.addEventListener('click', function(){
-    //     console.log('btnPlayPause')
-    // })
+        btnPlayPause.classList.toggle('paused')
+        btnPlayPause.classList.toggle('playing')
+    }
+
+    var skipTrack = function(ev){
+        scPlayer.play({playlistIndex: activeTrack})
+
+        btnTrackArr.forEach(function(element){ element.classList.remove('active') })
+        btnTrackArr[activeTrack].classList.add('active')
+
+        btnPlayPause.classList.remove('paused')
+        btnPlayPause.classList.add('playing')
+        isPlaying = 1
+    }
+
+
+    scPlayer.resolve(url, function (playlist) {
+        // bind play-pause
+        var mc = new Hammer(btnPlayPause)
+        mc.on("tap press", playPause)
+
+        // bind track-list
+        btnTrackArr.forEach(function(element, index){
+            activeTrack = index
+            var mc = new Hammer(element)
+            mc.on("tap press", skipTrack)
+        })
+
+        // bind end-track / play-all
+        scPlayer.on('ended', function() { scPlayer.next() })
+
+        // display play-pause
+        btnPlayPause.style.opacity = 1
+    });
 }
 
 },{"hammerjs":1,"soundcloud-audio":2}]},{},[3]);
